536bed87f4b0722ce35a0b6b96d88feb1b717b9f
beyond88 <muhin.cse.diu@gmail.com>
2024-03-03 22:04:15 +0600

    added auth all class

---
+++ app/Http/Controllers/auth/NewPasswordController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use App\Http\Controllers\Controller;
+use Elementor\Core\Utils\Str;
+use Give\Framework\Http\Response\Types\RedirectResponse;
+use GiveP2P\P2P\View;
+use Illuminate\Http\Request;
+use NotificationX\Core\Rules;
+
+class NewPasswordController extends Controller
+{
+    //
+
+    public function create(Request $request): View
+    {
+        return view('auth.reset-password', ['request' => $request]);
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'token' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed', Rules\Password::default()],
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user) use ($request) {
+                $user->forceFill([
+                    'password' => Hash::make($request->password),
+                    'remember_token' => Str::random(60),
+                ])->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status == Password::PASSWORD_RESET
+            ? redirect()->route('login')->with('status', __($status))
+            : back()->withInput($request->only('email'))
+                ->withErrors(['email' => __($status)]);
+    }
+}

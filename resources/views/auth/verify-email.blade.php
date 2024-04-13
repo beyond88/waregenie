67ea05de1c85c496c778b7cc9ece97811ffcf2c9
mohiuddin-ct <muhin.cse.diu@gmail.com>
2024-04-14 01:54:35 +0600

    add verify-email blade

---
+++ resources/views/auth/verify-email.blade.php
@@ -0,0 +1,55 @@
+@extends("layouts.layout")
+@section("title", "Verify Email | WareGenie")
+@section("content")
+
+    <div class="container-scroller">
+        <div class="container-fluid page-body-wrapper full-page-wrapper">
+            <div class="content-wrapper d-flex align-items-center auth">
+                <div class="row flex-grow">
+                    <div class="col-lg-4 mx-auto">
+                        <div class="auth-form-light text-left p-5">
+                            <div class="brand-logo">
+                                <img src="{{ asset('images/waregenie-logo.png') }}">
+                            </div>
+                            <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+                                {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+                            </div>
+
+                            @if (session('status') == 'verification-link-sent')
+                                <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
+                                    {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                                </div>
+                            @endif
+
+                            @if (session('error'))
+                                <div class="alert alert-danger" role="alert">
+                                    {{ session('error') }}
+                                </div>
+                            @endif
+
+                            <form method="POST" action="{{ route('verification.send') }}">
+                                @csrf
+
+                                <div>
+                                    <x-primary-button>
+                                        {{ __('Resend Verification Email') }}
+                                    </x-primary-button>
+                                </div>
+                            </form>
+
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <button type="submit" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+                                    {{ __('Log Out') }}
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- content-wrapper ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+    </div>
+@endsection
